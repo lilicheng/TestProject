95e64054641b55f0bdb73c40311be3b5f6bc2d6c
lilicheng <lilei_new@163.com>
2012-03-16 11:35:45 +0800

    create program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs ={ "aa", "bb", "cc" };
+
+            foreach (var tmp in strs.Where(n=>n.Length==2))
+                Console.Write(tmp);
+            Console.ReadLine();
+            
+        }
+    }
+}
